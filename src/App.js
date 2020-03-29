7a16d97079d3a7f7b9e46668d0e94e4bff3fb1f0
keeborg <ibortsov14@gmail.com>
2020-03-30 01:17:20 +0300

    Boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './vk_datetime_picker/VkDatetimePicker';
+import VkDatetimePicker from './vk_datetime_picker/VkDatetimePicker';
+
+function App() {
+
+    return (
+        <div className="App">
+            <VkDatetimePicker />
+        </div>
+    );
+}
+
+export default App;
